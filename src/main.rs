e7b7133fdc63facb2faf27aadc78db237e9bfeb3
david-soto-m <davidsotomarchena@protonmail.com>
2022-04-11 10:11:00 +0200

    Change bool to result, clippy, fmt

--- src/main.rs
+++ src/main.rs
@@ -267,15 +267,25 @@ impl EventObserver for KeyLogger {
 	}
 }
 
-fn check_uinput_loaded() -> bool {
-	let mut kernel_version = std::fs::read_to_string("/proc/version").unwrap();
-	kernel_version = kernel_version.to_string().split(' ').nth(2).unwrap().to_string();
 
-	let built_in_modules = std::fs::read_to_string(String::from("/lib/modules/") + &kernel_version + &String::from("/modules.builtin")).unwrap();
+fn check_uinput_loaded() -> std::result::Result<(),&'static str> {
+	let mut kernel_version = std::fs::read_to_string("/proc/version").unwrap();
+	kernel_version = kernel_version.split(' ')
+		.nth(2)
+		.unwrap()
+		.to_string();
+
+	let built_in_modules = std::fs::read_to_string(
+		String::from("/lib/modules/") + &kernel_version + &String::from("/modules.builtin"),
+	)
+	.unwrap();
 	for line in built_in_modules.lines() {
 		if line.eq("kernel/drivers/input/misc/uinput.ko") {
-			info!("'uinput' is built into running kernel version: {}", kernel_version);
-			return true;
+			info!(
+				"'uinput' is built into running kernel version: {}",
+				kernel_version
+			);
+			return Ok(());
 		}
 	}
 
@@ -283,20 +293,19 @@ fn check_uinput_loaded() -> bool {
 	for line in modules.lines() {
 		if line.starts_with("uinput ") {
 			info!("'uinput' module is loaded");
-			return true;
+			return Ok(());
 		}
 	}
-	return false;
+	Err("'uinput' module must be loaded OR built into the kernel")
 }
 
 fn start_mapper_from_file_conf(config_file: String) -> Result<()> {
-	if !check_uinput_loaded() {
-		panic!("'uinput' module must be loaded OR built into the kernel");
-	}
+	check_uinput_loaded().unwrap();
 	let config = serde_yaml::from_str(
 		&*std::fs::read_to_string(config_file.as_str())
-			.expect(&format!("Could not open file {}", config_file)))
-		.expect("Could not parse the configuration yaml file");
+			.expect(&format!("Could not open file {}", config_file)),
+	)
+	.expect("Could not parse the configuration yaml file");
 	start_mapper(config)
 }
 
